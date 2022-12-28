95bbdb6f0b3c03cb0cbf649b2b57e544049ad3ae
evgenyTarasovRepo <eatarasov@yahoo.com>
2022-12-28 14:44:39 +0300

    Initial commit

---
+++ src/test/java/com/qaguru/tarasov/gitlissonproject/MainClass.java
@@ -0,0 +1,18 @@
+package com.qaguru.tarasov.gitlissonproject;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainClass {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    @Test
+    public void testSum() {
+        int numberOne = 7;
+        int numberTwo = 7;
+
+        Assertions.assertEquals(14, sum(numberOne, numberTwo));
+    }
+}
